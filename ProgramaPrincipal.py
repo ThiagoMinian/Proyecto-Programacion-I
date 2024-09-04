654d54903855c6c9667231ffdeeb6efded679739
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2024-09-04 10:35:41 -0300

    Update main

--- ProgramaPrincipal.py
+++ ProgramaPrincipal.py
@@ -10,7 +10,7 @@ def CrearListaFechas(ListaFechas,Emparej,ce):
     fila = random.randint(0,ce-1)
     columna = random.randint(0,ce-1)
     k = 0
-    while k < (ce*ce) - 4:
+    while k < (ce*ce) :
         partido = Emparej[fila][columna]
         if ListaFechas.count(partido) == 0 and partido != "X":
             ListaFechas.append(partido)
@@ -40,19 +40,31 @@ def mostrar_resultados(lista1, lista2, listanombres):
         print(f"FECHA {i+1}: {listanombres[eq1]} {resultado_eq1} - {resultado_eq2} {listanombres[eq2]}")
  
 # Listas de ejemplo
-equipos = ["UBA", "UADE", "UTN", "UCA"] #validar equipos AL principio(pendiente)
+
+
 #lista1 = [1, 4, 2, 1, 3, 1, 1, 3, 1, 2, 1, 4]
 #lista2 = [2, 3, 4, 3, 2, 4, 3, 2, 4, 4, 2, 3]
 
-cant = 4
+cant = int(input("Ingrese la cantidad de equipos que participaran: "))
+cantidad = 0
+equipos = []
+while cantidad < cant:
+    equi = input("Ingrese las universidades que van a jugar ")
+    if equi.isalpha()==True:
+        equipos.append(equi)
+        cantidad +=1
+    else:
+        print("Tu contenido tiene un carácter no referenciado a una letra")
 listav = []
 matriz = []
 CrearMatrizEmparejamiento(matriz,cant)
+print(matriz)
 CrearListaFechas(listav, matriz,cant)
+print(listav)
 equipo1, equipo2 = dividir_matriz(listav)
-#imprimirlista(listav)
-#imprimirlista(equipo1)
-#imprimirlista(equipo2)
+imprimirlista(listav)
+imprimirlista(equipo1)
+imprimirlista(equipo2)
 mostrar_resultados(equipo1, equipo2, equipos)
 
 
