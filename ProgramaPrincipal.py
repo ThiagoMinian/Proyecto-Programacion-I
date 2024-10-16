66eb1cd304e664b89d4632d39a73970d479310f9
LeanCorti <leancorti2006@gmail.com>
2024-10-16 11:17:25 -0300

    edit_simfechas

--- ProgramaPrincipal.py
+++ ProgramaPrincipal.py
@@ -13,7 +13,10 @@ def dividir_matriz(lista):
         lista2.append(numero % 10)
 
     return lista1, lista2
-
+año = int(input("Ingrese el año en el que se juega el torneo"))
+mes = int(input("Ingrese el mes en el que se juega el torneo"))
+dia = int(input("Inrese el día en el que se juega el torneo"))
+fecha = dia + mes + año
 cant = int(input("Ingrese la cantidad de equipos que participaran (mínimo 2): "))
 cantidadEquipos = 0
 Nombresequipos = []
@@ -36,7 +39,8 @@ CrearMatrizEmparejamiento(matriz,cant)
 CrearListaFechas(listafech, matriz,cant)
 listEquipos1, listEquipos2 = dividir_matriz(listafech)
 mostrar_fechas(listEquipos1, listEquipos2, Nombresequipos)
-simfechas(listEquipos1, listEquipos2, Nombresequipos)
+simfechas(listEquipos1, listEquipos2, Nombresequipos, fecha)
+
 
  
 
--- SimFechas.py
+++ SimFechas.py
@@ -1,69 +1,80 @@
 
 import random
 
-def simfechas(lista1, lista2, listanombres):
+def simfechas(lista1, lista2, listanombres,fech):
     # Inicializar las listas de puntos, victorias, derrotas, empates, anotaciones y anotaciones recibidas
-    puntos = [0] * len(listanombres)
-    victorias = [0] * len(listanombres)
-    derrotas = [0] * len(listanombres)
-    empates = [0] * len(listanombres)
-    anotaciones_totales = [0] * len(listanombres)
-    anotaciones_recibidas = [0] * len(listanombres)
-    partidos_jugados = [0] * len(listanombres)
-    for i in range(len(lista1)):
-        eq1 = lista1[i] - 1
-        eq2 = lista2[i] - 1
-        # Generar resultados aleatorios para cada equipo
-        resultado_eq1 = random.randint(60, 100)
-        resultado_eq2 = random.randint(60, 100)
-        # Actualizar puntos, victorias, derrotas, empates, anotaciones y anotaciones recibidas según el resultado
-        if resultado_eq1 > resultado_eq2:
-            puntos[eq1] += 3
-            victorias[eq1] += 1
-            derrotas[eq2] += 1
-        elif resultado_eq1 < resultado_eq2:
-            puntos[eq2] += 3
-            victorias[eq2] += 1
-            derrotas[eq1] += 1
-        else:
-            puntos[eq1] += 1
-            puntos[eq2] += 1
-            empates[eq1] += 1
-            empates[eq2] += 1
-        # Actualizar anotaciones totales y recibidas
-        anotaciones_totales[eq1] += resultado_eq1
-        anotaciones_totales[eq2] += resultado_eq2
-        anotaciones_recibidas[eq1] += resultado_eq2
-        anotaciones_recibidas[eq2] += resultado_eq1
-        # Actualizar partidos jugados
-        partidos_jugados[eq1] += 1
-        partidos_jugados[eq2] += 1
-        # Imprimir el resultado del partido
-        print(f"FECHA {i+1}: {listanombres[eq1]} {resultado_eq1} - {resultado_eq2} {listanombres[eq2]}")
-        # Crear una lista de tuplas (equipo, puntos, partidos jugados, victorias, empates, derrotas)
-        tabla_puntos = list(zip(listanombres, puntos, partidos_jugados, victorias, empates, derrotas))
-        # Ordenar la tabla de puntos por puntos en orden descendente
-        tabla_puntos_ordenada = sorted(tabla_puntos, key=lambda x: x[1], reverse=True)
-        # Imprimir la tabla de puntos actualizada con encabezados
-        print("Tabla de puntos:")
-        print(f"{'Equipo':<10} {'Puntos':<6} {'PJ':<3} {'V':<3} {'E':<3} {'D':<3}")
-        for equipo, punto, pj, victoria, empate, derrota in tabla_puntos_ordenada:
-            print(f"{equipo:<10} {punto:<6} {pj:<3} {victoria:<3} {empate:<3} {derrota:<3}")
+    try:
+        puntos = [0] * len(listanombres)
+        victorias = [0] * len(listanombres)
+        derrotas = [0] * len(listanombres)
+        empates = [0] * len(listanombres)
+        anotaciones_totales = [0] * len(listanombres)
+        anotaciones_recibidas = [0] * len(listanombres)
+        partidos_jugados = [0] * len(listanombres)
+        for i in range(len(lista1)):
+            eq1 = lista1[i] - 1
+            eq2 = lista2[i] - 1
+            # Generar resultados aleatorios para cada equipo
+            resultado_eq1 = random.randint(60, 100)
+            resultado_eq2 = random.randint(60, 100)
+            # Actualizar puntos, victorias, derrotas, empates, anotaciones y anotaciones recibidas según el resultado
+            if resultado_eq1 > resultado_eq2:
+                puntos[eq1] += 3
+                victorias[eq1] += 1
+                derrotas[eq2] += 1
+            elif resultado_eq1 < resultado_eq2:
+                puntos[eq2] += 3
+                victorias[eq2] += 1
+                derrotas[eq1] += 1
+            else:
+                puntos[eq1] += 1
+                puntos[eq2] += 1
+                empates[eq1] += 1
+                empates[eq2] += 1
+            # Actualizar anotaciones totales y recibidas
+            anotaciones_totales[eq1] += resultado_eq1
+            anotaciones_totales[eq2] += resultado_eq2
+            anotaciones_recibidas[eq1] += resultado_eq2
+            anotaciones_recibidas[eq2] += resultado_eq1
+            # Actualizar partidos jugados
+            partidos_jugados[eq1] += 1
+            partidos_jugados[eq2] += 1
+            # Imprimir el resultado del partido
+            print(f"FECHA {i+1}: {listanombres[eq1]} {resultado_eq1} - {resultado_eq2} {listanombres[eq2]}")
+            # Crear una lista de tuplas (equipo, puntos, partidos jugados, victorias, empates, derrotas)
+            tabla_puntos = list(zip(listanombres, puntos, partidos_jugados, victorias, empates, derrotas))
+            # Ordenar la tabla de puntos por puntos en orden descendente
+            tabla_puntos_ordenada = sorted(tabla_puntos, key=lambda x: x[1], reverse=True)
+            # Imprimir la tabla de puntos actualizada con encabezados
+            print("Tabla de puntos:")
+            print(f"{'Equipo':<10} {'Puntos':<6} {'PJ':<3} {'V':<3} {'E':<3} {'D':<3}")
+            for equipo, punto, pj, victoria, empate, derrota in tabla_puntos_ordenada:
+                print(f"{equipo:<10} {punto:<6} {pj:<3} {victoria:<3} {empate:<3} {derrota:<3}")
+            print()
+        # Imprimir la tabla final con Campeón y Subcampeón
+        nombre_archivo = "Torneo" + str(fech) + ".csv"
+        arch = open(nombre_archivo, "wt")
+        print("Tabla final:")
+        print(f"{'Equipo':<10} {'Puntos':<6} {'PJ':<3} {'V':<3} {'E':<3} {'D':<3} {'Anot Tot':<10} {'Anot Rec':<10} {'Dif Anot':<10}")
+        for i in range(len(tabla_puntos_ordenada)):
+            equipo, punto, pj, victoria, empate, derrota = tabla_puntos_ordenada[i]
+            anotaciones_tot = anotaciones_totales[listanombres.index(equipo)]
+            anotaciones_rec = anotaciones_recibidas[listanombres.index(equipo)]
+            diferencia_anot = anotaciones_tot - anotaciones_rec
+            if i == 0:
+                titulo = "Campeón"
+            elif i == 1:
+                titulo = "Subcampeón"
+            else:
+                titulo = ""
+            print(f"{equipo:<10} {punto:<6} {pj:<3} {victoria:<3} {empate:<3} {derrota:<3} {anotaciones_tot:<10} {anotaciones_rec:<10} {diferencia_anot:<10} {titulo}")
+            información = equipo + (";") + str(punto) + (";") + str(pj) + (";") + str(victoria) + (";") + str(empate) + (";") + str(derrota) + (";") + str(anotaciones_tot) + (";") + str(anotaciones_rec) + (";") + str(diferencia_anot)
+            arch.write(información)
+            arch.close
+            print("Archivo con los datos del torneo generado correctamente")
+
         print()
-    # Imprimir la tabla final con Campeón y Subcampeón
-    print("Tabla final:")
-    print(f"{'Equipo':<10} {'Puntos':<6} {'PJ':<3} {'V':<3} {'E':<3} {'D':<3} {'Anot Tot':<10} {'Anot Rec':<10} {'Dif Anot':<10}")
-    for i in range(len(tabla_puntos_ordenada)):
-        equipo, punto, pj, victoria, empate, derrota = tabla_puntos_ordenada[i]
-        anotaciones_tot = anotaciones_totales[listanombres.index(equipo)]
-        anotaciones_rec = anotaciones_recibidas[listanombres.index(equipo)]
-        diferencia_anot = anotaciones_tot - anotaciones_rec
-        if i == 0:
-            titulo = "Campeón"
-        elif i == 1:
-            titulo = "Subcampeón"
-        else:
-            titulo = ""
-        print(f"{equipo:<10} {punto:<6} {pj:<3} {victoria:<3} {empate:<3} {derrota:<3} {anotaciones_tot:<10} {anotaciones_rec:<10} {diferencia_anot:<10} {titulo}")
-    print()
- 
+    except IOError:
+        print("No se pudo generar el archivo del torneo")
+    except FileNotFoundError:
+        print("No se encontró el archivo para crear")
